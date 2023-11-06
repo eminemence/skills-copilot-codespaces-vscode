8a20b333a35afb06c8f36b3011340570de483aae
eminemence <eminemence@gmail.com>
2023-11-06 06:52:40 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,40 +1,71 @@
-// create web server
-// 1. load modules
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-// 2. create server
-var server = http.createServer(function (request, response) {
-    // request: client -> server
-    // response: server -> client
+// Create web server application
+const express = require('express');
+const app = express();
 
-    // 3. parsing url
-    var parsedUrl = url.parse(request.url, true);
-    var query = parsedUrl.query;
-    var pathname = parsedUrl.pathname;
-
-    if (pathname === '/') {
-        // 4. read html file
-        fs.readFile('index.html', 'utf-8', function (error, data) {
-            // 5. response html
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(data);
-        });
-    } else if (pathname === '/create') {
-        fs.readFile('create.html', 'utf-8', function (error, data) {
-            response.writeHead(200, { 'Content-Type': 'text/html' });
-            response.end(data);
-        });
-    } else if (pathname === '/create_process') {
-        var body = '';
-        request.on('data', function (data) {
-            body += data;
-        }
-        );
-    }
+// Create connection to MySQL database
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    port: 8889, // Change this to your own port number
+    database: 'comments'
 });
-// 6. listen port
-server.listen(3000);
 
+// Start server
+const server = app.listen(8888, () => {
+    console.log('Server is running...');
+});
 
+// Set up socket.io
+const io = require('socket.io')(server);
+
+// Set up socket.io connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+
+    // Get all comments from database
+    connection.query('SELECT * FROM comments', (err, data) => {
+        if (err) {
+            console.log(err);
+            socket.emit('error', err);
+        } else {
+            socket.emit('output', data);
+        }
+    });
+
+    // Listen to 'input' event
+    socket.on('input', (data) => {
+        let name = data.name;
+        let comment = data.comment;
+        let timestamp = Math.floor(Date.now() / 1000);
+
+        // Insert data into database
+        connection.query('INSERT INTO comments (name, comment, timestamp) VALUES (?, ?, ?)', [name, comment, timestamp], (err, data) => {
+            if (err) {
+                console.log(err);
+                socket.emit('error', err);
+            } else {
+                io.emit('output', [data]); // Send back data to all users
+            }
+        });
+    });
+
+    // Listen to 'clear' event
+    socket.on('clear', () => {
+        // Delete all data from database
+        connection.query('DELETE FROM comments', (err) => {
+            if (err) {
+                console.log(err);
+                socket.emit('error', err);
+            } else {
+                io.emit('cleared'); // Send back message to all users
+            }
+        });
+    });
+
+    // Listen to 'disconnect' event
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
